f8244382db2eb075e722bacaeae8fefe34079ff1
CeausMaxim <chaushm@mail.ru>
2021-10-05 12:35:52 +0300

    Initial commit

---
+++ src/main/java/com/example/dbhibernatemysqlmigration/DbHibernateMySqlMigrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbhibernatemysqlmigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbHibernateMySqlMigrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbHibernateMySqlMigrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dbhibernatemysqlmigration/DbHibernateMySqlMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbhibernatemysqlmigration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbHibernateMySqlMigrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
