8d353ef670bf22b6a06dd8be329d293c48253ba6
CeausMaxim <chaushm@mail.ru>
2021-10-05 12:51:38 +0300

    Add model Country

---
+++ src/main/java/com/example/dbhibernatemysqlmigration/model/Country.java
@@ -0,0 +1,20 @@
+package com.example.dbhibernatemysqlmigration.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "countries")
+public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "code")
+    private String code;
+}
